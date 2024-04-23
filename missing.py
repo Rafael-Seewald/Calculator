58cdbcdd22f660bf38c0751cf4e79d97ed1d75c8
Rafael Vinícius Seewald <rafa.vini.seewald@gmail.com>
2024-04-22 22:18:13 -0300

    Delete missing.py

--- missing.py
+++
@@ -1,7 +0,0 @@
-"""
-Falta adicionar calculos com números decimais
-Botão de raiz quadrada
-Botão de potenciação
-Botão de deletar último caractere da telinha da calculadora
-Configurar cálculos com "+*-" juntos
-"""
